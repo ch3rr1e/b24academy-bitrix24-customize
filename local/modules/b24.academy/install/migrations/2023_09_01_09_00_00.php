fa4ad37286ad3ca15f1cd093706b164ab6992630
ch3rr1e <nikatofu@ya.ru>
2024-03-10 18:55:38 +0300

    Remove old files

--- local/modules/b24.academy/install/migrations/2023_09_01_09_00_00.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Bitrix\Main\Config\Option;
-use Bitrix\Main\ModuleManager;
-
-ModuleManager::registerModule('b24.academy');
-
-Option::set('b24.academy', 'VERSION', 2023_09_01_09_00_00);
